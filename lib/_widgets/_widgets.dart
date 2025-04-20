e15f7cf16ab117576fbade1bd7ed9a570c921f34
Ahnaf Sakil <55395335+Ahnaf16@users.noreply.github.com>
2025-04-20 16:46:05 +0600

    ..

--- lib/_widgets/_widgets.dart
+++ lib/_widgets/_widgets.dart
@@ -1,6 +1,6 @@
 export 'alignments.dart';
 export 'flex/separated_flex.dart';
 export 'hosted_image.dart';
+export 'limited_width_box.dart';
 export 'loader.dart';
-export 'responsive_width_box.dart';
 export 'swipe.dart';
--- lib/_widgets/limited_width_box.dart
+++ lib/_widgets/limited_width_box.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
-class ResponsiveWidthBox extends StatelessWidget {
-  const ResponsiveWidthBox({super.key, required this.child, required this.maxWidth, this.padding});
+class LimitedWidthBox extends StatelessWidget {
+  const LimitedWidthBox({super.key, required this.child, required this.maxWidth, this.padding, this.center = true});
 
   final Widget child;
   final double maxWidth;
   final EdgeInsetsGeometry? padding;
+  final bool center;
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +15,9 @@ class ResponsiveWidthBox extends StatelessWidget {
         final screenWidth = constraints.maxWidth;
         final width = screenWidth > maxWidth ? maxWidth : screenWidth;
 
-        return Center(child: Container(width: width, padding: padding, child: child));
+        final c = Container(width: width, padding: padding, child: child);
+
+        return center ? Center(child: c) : c;
       },
     );
   }
